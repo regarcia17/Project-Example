bd85a5d1839cd92404a0b4d1c28f2301f79215e5
Rebecca Garcia <rebeccagarcia@Rebeccas-MacBook-Pro-2.local>
2018-02-27 10:41:00 -0600

    Added interactive button

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+var visibleText = "Show Less";
+var hiddenText = "Show More";
+
 $(document).ready(function(){
-    alert("jQuery ready!");
+    //alert("jQuery ready!");
+    //.next mean next element
+    
+    $(".info-box button").click(function(){
+        $(this).toggleClass("visible");
+        
+        //example of using conditional logic
+        if($(this).hasClass("visible")){
+            $(this).html(visibleText);
+        }else{
+            $(this).html(hiddenText);
+        }
+        
+        $(this).next().toggleClass("visible");
+    });
 });
